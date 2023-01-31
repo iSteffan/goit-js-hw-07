11ce8346f234c114623083768fe344459e5e8ee6
Stepan Skichko <st.skichko@gmail.com>
2023-01-31 16:05:50 +0200

    homework 7, task 1 DONE

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery() {
+  return galleryItems
+    .map(item => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+galleryRef.innerHTML = createGallery(galleryItems);
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `
+	<img width="1280" src="${event.target.dataset.source}">
+`
+  );
+  instance.show();
+
+  galleryRef.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+});
