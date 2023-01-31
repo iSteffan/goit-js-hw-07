5c8d9073a7d72976af562e7a469372de79301723
Stepan Skichko <st.skichko@gmail.com>
2023-01-31 18:36:22 +0200

    task 2 IN PROGRESS

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery() {
+  return galleryItems
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryRef.innerHTML = createGallery(galleryItems);
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+});
